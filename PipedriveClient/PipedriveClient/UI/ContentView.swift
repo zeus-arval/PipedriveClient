b0ce4b37dfa0390ef08ea12dd564216251b0ebe0
Artur Valdna <arturvaldna@gmail.com>
2023-11-23 21:14:31 +0200

    #1 Added project template

---
+++ PipedriveClient/PipedriveClient/UI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PipedriveClient
+//
+//  Created by Артур Валдна on 23.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ PipedriveClient/PipedriveClient/UI/PipedriveClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PipedriveClientApp.swift
+//  PipedriveClient
+//
+//  Created by Артур Валдна on 23.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct PipedriveClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
